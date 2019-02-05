a9bc96dc0e6dda2a3933748cf73060d86d120554
Shad Sharma <shadanan@gmail.com>
2019-02-05 14:44:33 -0800

    Add a function to set the transpostion of the MD (#2)
    We can have a set of functions that will configure the renderer before the renderer is called. Added a section in the code where the renderer can be configured by adding the appropriate functions.

--- index.js
+++ index.js
@@ -2,20 +2,26 @@
 const abc = require('./abc_renderer.js');
 const chords = require('./chords_renderer.js');
 
-function MarkdownMusic(md, opts) {
+function MarkdownMusic(md, musicOpts) {
   md.highlightRegistry = {};
-  md.opts = opts;
+  md.musicOpts = musicOpts;
 
   md.set({
     highlight: function(str, lang) {
       if (md.highlightRegistry.hasOwnProperty(lang)) {
-        return md.highlightRegistry[lang](str, md.opts);
+        return md.highlightRegistry[lang](str, md.musicOpts);
       }
     }
   });
 
+  // Renderer registry
   md.highlightRegistry[abc.lang] = abc.callback;
   md.highlightRegistry[chords.lang] = chords.callback;
+
+  // Renderer configuration functions
+  md.setTranspose = function(transpose) {
+    md.musicOpts.transpose = transpose;
+  }
 };
 
 module.exports = MarkdownMusic;
