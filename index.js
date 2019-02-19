83af7468f85fd5c7ca3d172d94986af31638992a
Vince Lugli <vilugli@microsoft.com>
2019-02-19 14:27:51 -0800

    Organize files, start using renderer.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 const abc = require('./abc_renderer.js');
-const chords = require('./chords_renderer.js');
+const chords = require('./renderers/chords_renderer.js');
 
 function MarkdownMusic(md, musicOpts) {
   md.highlightRegistry = {};
