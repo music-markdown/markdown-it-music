fe6e45caa02195d1411be1c7bbba612d61d527c5
Vince Lugli <vilugli@microsoft.com>
2019-02-21 21:28:43 -0800

    Update failing tests.

--- parsers/verse.spec.js
+++ parsers/verse.spec.js
@@ -60,23 +60,27 @@ describe('Verse', () => {
       'l1: I didn\'t ask, you shouldn\'t have told me'
     ].join('\n');
     const actual = parsePhrase(phrase);
-    expect(actual).toEqual({
-      'c1': [
-        { index: 0, content: new Chord('G') },
-        { index: 9, content: new Chord('E', 'm') },
-        { index: 33, content: new Chord('B', 'm') },
+    expect(actual).toEqual(expect.objectContaining(new Map([
+      [
+        'c1', [
+          { index: 0, content: new Chord('G') },
+          { index: 9, content: new Chord('E', 'm') },
+          { index: 33, content: new Chord('B', 'm') },
+        ],
       ],
-      'l1': [
-        { index: 0, content: 'I' },
-        { index: 2, content: 'didn\'t' },
-        { index: 9, content: 'ask,' },
-        { index: 14, content: 'you' },
-        { index: 18, content: 'shouldn\'t' },
-        { index: 28, content: 'have' },
-        { index: 33, content: 'told' },
-        { index: 38, content: 'me' },
+      [
+        'l1', [
+          { index: 0, content: 'I' },
+          { index: 2, content: 'didn\'t' },
+          { index: 9, content: 'ask,' },
+          { index: 14, content: 'you' },
+          { index: 18, content: 'shouldn\'t' },
+          { index: 28, content: 'have' },
+          { index: 33, content: 'told' },
+          { index: 38, content: 'me' },
+        ]
       ]
-    });
+    ])));
   });
 
   test('parses verse with two phrases', () => {
@@ -88,35 +92,45 @@ describe('Verse', () => {
       'l1:       and the sky is gray.',
     ].join('\n');
     const actual = parseVerse(verse);
-    expect(actual).toEqual([
-      {
-        'c1': [
-          { index: 19, content: new Chord('A', 'm') },
+    debugger;
+    expect(new Map()).toEqual(new Map());
+    expect(actual).toEqual(expect.objectContaining([
+      new Map([
+        [
+          'c1', [
+            { index: 19, content: new Chord('A', 'm') },
+          ],
         ],
-        'l1': [
-          { index: 0, content: 'All' },
-          { index: 4, content: 'the' },
-          { index: 8, content: 'leaves' },
-          { index: 15, content: 'are' },
-          { index: 19, content: 'brown' },
+        [
+          'l1', [
+            { index: 0, content: 'All' },
+            { index: 4, content: 'the' },
+            { index: 8, content: 'leaves' },
+            { index: 15, content: 'are' },
+            { index: 19, content: 'brown' },
+          ]
         ]
-      },
-      {
-        'c1': [
-          { index: 0, content: new Chord('G') },
-          { index: 3, content: new Chord('F') },
-          { index: 14, content: new Chord('G') },
-          { index: 21, content: new Chord('E', 'sus2') },
-          { index: 27, content: new Chord('E') },
+      ]),
+      new Map([
+        [
+          'c1', [
+            { index: 0, content: new Chord('G') },
+            { index: 3, content: new Chord('F') },
+            { index: 14, content: new Chord('G') },
+            { index: 21, content: new Chord('E', 'sus2') },
+            { index: 27, content: new Chord('E') },
+          ],
         ],
-        'l1': [
-          { index: 6, content: 'and' },
-          { index: 10, content: 'the' },
-          { index: 14, content: 'sky' },
-          { index: 18, content: 'is' },
-          { index: 21, content: 'gray.' },
+        [
+          'l1', [
+            { index: 6, content: 'and' },
+            { index: 10, content: 'the' },
+            { index: 14, content: 'sky' },
+            { index: 18, content: 'is' },
+            { index: 21, content: 'gray.' },
+          ]
         ]
-      },
-    ]);
+      ])
+    ]));
   });
 });
