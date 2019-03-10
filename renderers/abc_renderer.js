e4b525d3ecdc1588da7893255d50df3155ee3caf
Shad Sharma <shadanan@gmail.com>
2019-03-09 22:38:55 -0800

    Make ABC.js music responsive to resize

--- renderers/abc_renderer.js
+++ renderers/abc_renderer.js
@@ -4,7 +4,13 @@ const abc = require('abcjs');
 function renderAbc(str, opts) {
   const div = document.createElement('div');
   abc.renderAbc(div, str, {
-    visualTranspose: opts.transpose
+    visualTranspose: opts.transpose,
+    staffwidth: opts.maxWidth,
+    paddingtop: 0,
+    paddingbottom: 0,
+    paddingright: 0,
+    paddingleft: 0,
+    wrap: { minSpacing: 1.8, maxSpacing: 2.7 },
   });
   return div.outerHTML;
 }
