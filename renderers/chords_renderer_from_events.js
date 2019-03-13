90c86de3f8382e1726f86b8ffc79cc7b47767a59
Vince Lugli <vilugli@microsoft.com>
2019-03-12 17:37:15 -0700

    Fix couple of font-size bugs.

--- renderers/chords_renderer_from_events.js
+++ renderers/chords_renderer_from_events.js
@@ -11,8 +11,8 @@ class ChordsEventRenderer {
     this.voiceOrder = voiceOrder;
     this.voiceColors = new VoiceColors(colorOrder);
     this.transposeAmount = opts ? opts.transpose : undefined;
-    this.columnCount = opts ? opts.columnCount : 1;
-    this.fontSize = opts ? opts.fontSize : 13;
+    this.columnCount = opts && opts.columnCount ? opts.columnCount : 1;
+    this.fontSize = opts && opts.fontSize ? opts.fontSize : 13;
   }
 
   createEventHTMLChordChart(lines) {
@@ -20,7 +20,7 @@ class ChordsEventRenderer {
     const chartDiv = document.createElement('div');
     chartDiv.className = 'chart';
     chartDiv.style.columnCount = this.columnCount;
-    chartDiv.style.fontSize = this.fontSize;
+    chartDiv.style.fontSize = `${this.fontSize}px`;
 
     // create events div for each line list
     lines.forEach((line) => {
--- renderers/chords_renderer_from_events.spec.js
+++ renderers/chords_renderer_from_events.spec.js
@@ -109,7 +109,7 @@ describe('Chords Renderer from Events', () => {
     const chordsRenderer = new ChordsEventRenderer(['c1', 'l1'], voiceColors);
     const actualChartDiv = chordsRenderer.createEventHTMLChordChart(lines);
 
-    const expectedEventDiv = '<div class="chart" style="column-count: 1; font-size: 13;">' +
+    const expectedEventDiv = '<div class="chart" style="column-count: 1; font-size: 13px;">' +
       '<div class="line">' +
         '<div class="event">' +
           `<div style="color: ${defaultColors[0]};">` +
@@ -208,7 +208,7 @@ describe('Chords Renderer from Events', () => {
   });
 
   test('should add columns', () => {
-    const expectedChartDiv = '<div class="chart" style="column-count: 3;">' +
+    const expectedChartDiv = '<div class="chart" style="column-count: 3; font-size: 13px;">' +
       '<div class="line">' +
         '<div class="event">' +
           `<div style="color: ${defaultColors[0]};">` +
@@ -233,7 +233,7 @@ describe('Chords Renderer from Events', () => {
   });
 
   test('should add font size', () => {
-    const expectedChartDiv = '<div class="chart" style="font-size: 999;">' +
+    const expectedChartDiv = '<div class="chart" style="column-count: 1; font-size: 999px;">' +
       '<div class="line">' +
         '<div class="event">' +
           `<div style="color: ${defaultColors[0]};">` +
