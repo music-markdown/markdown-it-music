652208a00cba521e4ba4a59c0743071224c151b7
Shad Sharma <shadanan@gmail.com>
2019-03-16 14:56:39 -0700

    Tweak right margin of abc renderer

--- renderers/abc_renderer.js
+++ renderers/abc_renderer.js
@@ -1,11 +1,12 @@
 'use strict';
 const abc = require('abcjs');
+const MARGIN_RIGHT = 2;
 
 function renderAbc(str, opts) {
   const div = document.createElement('div');
   abc.renderAbc(div, str, {
     visualTranspose: opts.transpose,
-    staffwidth: opts.maxWidth,
+    staffwidth: opts.maxWidth - MARGIN_RIGHT,
     paddingtop: 0,
     paddingbottom: 0,
     paddingright: 0,
