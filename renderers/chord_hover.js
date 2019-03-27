0f15380e2fe85d68b161286992b7c2656ecac0b5
Vince Lugli <vilugli@microsoft.com>
2019-03-26 22:12:01 -0700

    Select different diagrams for chords.

--- renderers/chord_hover.js
+++ renderers/chord_hover.js
@@ -3,19 +3,103 @@
 const chordDiagram = require('./chord_diagram.js');
 const { guitarChordLibrary } = require('./chord_library.js');
 
+const slowScrollFunction = `
+function slowScroll(id, direction, distance, step) {
+  var element = document.getElementById(id);
+  
+  if (element) {
+    var scrollAmount = 0;
+    var slideTimer = setInterval(function() {
+      element.scrollLeft += direction === 'left' ? -step : step;
+
+      scrollAmount += step;
+      if(scrollAmount >= distance){
+          window.clearInterval(slideTimer);
+      }
+    });
+  }
+}`;
+
+const incrementIdFunction = `
+function incrementId(id, direction) {
+  var countId = id + '-count'
+  var element = document.getElementById(countId);
+
+  if (element) {
+    var values = element.innerHTML.split(' of ');
+    var currentValue = parseInt(values[0]);
+
+    if (direction === 'left' && currentValue > 1) {
+      element.innerHTML = currentValue - 1;
+      element.innerHTML += ' of ' + values[1];
+    } else if (direction === 'right' && currentValue < parseInt(values[1])) {
+      element.innerHTML = currentValue + 1;
+      element.innerHTML += ' of ' + values[1];
+    }
+  }
+}
+`;
+
+function createDiagramDiv(voicing, chordDiagramContainerDiv) {
+  const svg = chordDiagram.renderChordDiagram(voicing);
+
+  const chordDiagramDiv = document.createElement('div');
+
+  chordDiagramDiv.className = 'diagram';
+  chordDiagramDiv.innerHTML += svg;
+
+  chordDiagramContainerDiv.appendChild(chordDiagramDiv);
+}
+
 function addChordToDiv(voiceDiv, chord) {
   voiceDiv.className += ' chord';
 
   if (guitarChordLibrary.has(chord.toString())) {
-    const chordDiagramDiv = document.createElement('div');
-    chordDiagramDiv.className = 'diagram';
+    const containerDiv = document.createElement('div');
+    containerDiv.className = 'diagram-container';
+
+    const contentDiv = document.createElement('div');
+    contentDiv.className = 'diagram-content-container';
+
+    const id = 'chord' + Math.floor(Math.random() * 10000);
+    contentDiv.id = id;
+
+    if (!document.getElementById('chord-hover-script')) {
+      const script = document.createElement('script');
+      script.id = 'chord-hover-script';
+      script.innerHTML = `${slowScrollFunction}${incrementIdFunction}`;
+
+      document.getElementsByTagName('body')[0].appendChild(script);
+    }
+
+    const contentScrollDiv = document.createElement('div');
+    contentScrollDiv.className = 'content-scroll';
+
+    const leftButton = document.createElement('div');
+    leftButton.innerHTML = `<button class="scroll"
+      onclick="slowScroll('${id}', 'left', 100, 2); incrementId('${id}', 'left');">❮`;
+
+    const rightButton = document.createElement('div');
+    rightButton.innerHTML = `<button class="scroll" 
+      onclick="slowScroll('${id}', 'right', 100, 2); incrementId('${id}', 'right');">❯`;
+
+    contentScrollDiv.appendChild(leftButton);
+    contentScrollDiv.appendChild(contentDiv);
+    contentScrollDiv.appendChild(rightButton);
 
     const voicings = guitarChordLibrary.get(chord.toString());
-    const svgs = voicings.map((voicing) => chordDiagram.renderChordDiagram(voicing));
 
-    // TODO: Provide a way to scroll through several chord diagrams.
-    chordDiagramDiv.innerHTML = svgs[0];
-    voiceDiv.appendChild(chordDiagramDiv);
+    voicings.forEach((voicing) => {
+      createDiagramDiv(voicing, contentDiv);
+    });
+
+    const tempDiv = document.createElement('div');
+    tempDiv.id = `${id}-count`;
+    tempDiv.innerHTML = `1 of ${voicings.length}`;
+    containerDiv.appendChild(contentScrollDiv);
+    containerDiv.appendChild(tempDiv);
+
+    voiceDiv.appendChild(containerDiv);
   } else {
     voiceDiv.className += ' highlight';
   }
--- renderers/chord_hover.spec.js
+++ renderers/chord_hover.spec.js
@@ -2,21 +2,59 @@
 
 const rewire = require('rewire');
 
+const mockMath = Object.create(global.Math);
+mockMath.random = () => 0.5;
+
 const chordHoverJs = rewire('./chord_hover.js');
 chordHoverJs.__set__('document', document);
+chordHoverJs.__set__('Math', mockMath);
 chordHoverJs.__get__('chordDiagram').renderChordDiagram = jest.fn(() => 'svg_here');
 
 const addChordToDiv = chordHoverJs.__get__('addChordToDiv');
+const createDiagramDiv = chordHoverJs.__get__('createDiagramDiv');
+const slowScrollFunction = chordHoverJs.__get__('slowScrollFunction');
+const incrementIdFunction = chordHoverJs.__get__('incrementIdFunction');
+
+jest.useFakeTimers();
 
 const svgDiagramDiv = '<div class="diagram">svg_here</div>';
 
 describe('Chord hover', () => {
-  test('should add diagram and chord class', () => {
+  test('should add diagram to container', () => {
+    const voiceDiv = document.createElement('div');
+
+    createDiagramDiv('voicing', voiceDiv);
+
+    const expectedVoiceDiv = `<div>${svgDiagramDiv}</div>`;
+    expect(voiceDiv.outerHTML).toEqual(expectedVoiceDiv);
+  });
+  test('should add diagram and chord class with navigation', () => {
     const voiceDiv = document.createElement('div');
 
     addChordToDiv(voiceDiv, 'C');
 
-    const expectedVoiceDiv = `<div class=" chord">${svgDiagramDiv}</div>`;
+    const expectedVoiceDiv = `<div class=" chord">` +
+      `<div class="diagram-container">` +
+        `<div class="content-scroll">` +
+          `<div>` +
+            `<button class="scroll" ` +
+              `onclick="slowScroll('chord5000', 'left', 100, 2); incrementId('chord5000', 'left');">` +
+                `❮` +
+            `</button>` +
+          `</div>` +
+          `<div class="diagram-content-container" id="chord5000">` +
+            `${svgDiagramDiv}${svgDiagramDiv}${svgDiagramDiv}` +
+          `</div>` +
+          `<div>` +
+            `<button class="scroll" ` +
+              `onclick="slowScroll('chord5000', 'right', 100, 2); incrementId('chord5000', 'right');">` +
+                `❯` +
+            `</button>` +
+          `</div>` +
+        `</div>` +
+        `<div id="chord5000-count">1 of 3</div>` +
+      `</div>` +
+    `</div>`;
 
     expect(voiceDiv.outerHTML).toEqual(expectedVoiceDiv);
   });
@@ -29,5 +67,72 @@ describe('Chord hover', () => {
     const expectedVoiceDiv = '<div class=" chord highlight"></div>';
 
     expect(voiceDiv.outerHTML).toEqual(expectedVoiceDiv);
+  });
+
+  describe('buttons', () => {
+    let voiceDiv;
+    const id = 'chord5000';
+
+    beforeAll(() => {
+      eval(slowScrollFunction);
+      eval(incrementIdFunction);
+    });
+
+    beforeEach(() => {
+      if (voiceDiv) {
+        document.getElementsByTagName('body')[0].removeChild(voiceDiv);
+      }
+      voiceDiv = document.createElement('div');
+
+      addChordToDiv(voiceDiv, 'C');
+
+      document.getElementsByTagName('body')[0].appendChild(voiceDiv);
+    });
+
+    test('buttons should scroll div right', () => {
+      const scrollDistance = 100;
+      slowScroll(id, 'right', scrollDistance, 2);
+      jest.advanceTimersByTime(1000);
+
+      expect(document.getElementById(id).scrollLeft).toEqual(scrollDistance);
+    });
+
+    test('buttons should scroll div right then left, to get back to original localtion', () => {
+      const scrollDistance = 100;
+      slowScroll(id, 'right', scrollDistance, 2);
+      jest.advanceTimersByTime(100);
+
+      slowScroll(id, 'left', scrollDistance, 2);
+      jest.advanceTimersByTime(100);
+
+      expect(document.getElementById(id).scrollLeft).toEqual(0);
+    });
+
+    test('buttons should increment values when scrolling right', () => {
+      incrementId(id, 'right');
+
+      expect(document.getElementById(`${id}-count`).innerHTML).toEqual('2 of 3');
+    });
+
+    test('buttons should decrement values when scrolling left', () => {
+      incrementId(id, 'right');
+      incrementId(id, 'left');
+
+      expect(document.getElementById(`${id}-count`).innerHTML).toEqual('1 of 3');
+    });
+
+    test('buttons should not increment over the max value', () => {
+      incrementId(id, 'right');
+      incrementId(id, 'right');
+      incrementId(id, 'right');
+
+      expect(document.getElementById(`${id}-count`).innerHTML).toEqual('3 of 3');
+    });
+
+    test('buttons should not decrement below zero', () => {
+      incrementId(id, 'left');
+
+      expect(document.getElementById(`${id}-count`).innerHTML).toEqual('1 of 3');
+    });
   });
 });
