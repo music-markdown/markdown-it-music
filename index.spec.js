cfc10ac403c2068e8236b357bcbae6259044f2c8
Shad Sharma <shadanan@gmail.com>
2020-04-24 11:48:08 -0700

    Support for custom chords in markdown header. (#93)

---
+++ index.spec.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const MarkdownIt = require("markdown-it");
+const MarkdownItMusic = require("./index");
+
+describe("Markdown It Music", () => {
+  var md;
+
+  beforeEach(() => {
+    md = new MarkdownIt({ html: true }).use(MarkdownItMusic);
+  });
+
+  test("YouTube ID is parsed if present", () => {
+    const expected = "1ClCpfeIELw";
+    md.render(`---
+youTubeId: ${expected}
+---`);
+    expect(md.meta.youTubeId).toEqual(expected);
+  });
+});
