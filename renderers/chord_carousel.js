6a3d86440740e035441ba566e7a03155828d3e45
Shad Sharma <shadanan@gmail.com>
2023-04-28 23:54:33 -0700

    Replace Popper.js with Floating-UI which supports zoom (#116)

--- renderers/chord_carousel.js
+++ renderers/chord_carousel.js
@@ -25,13 +25,13 @@ function chordCarousel(chord) {
 
   return (
     `<div id="tooltip-${attrValue}" class="tooltip"` +
-    ` onmouseover="cancelHidePopper('${attrValue}')"` +
-    ` onmouseout="hidePopper('${attrValue}')">` +
+    ` onmouseover="cancelHideTooltip('${attrValue}')"` +
+    ` onmouseout="hideTooltip('${attrValue}')">` +
     `<div class="chord-carousel">` +
     carousel +
     `<div id="${id}-count">1 of ${voicings.length}</div>` +
     `</div>` +
-    `<div class="arrow" data-popper-arrow></div>` +
+    `<div id="arrow-${attrValue}" class="arrow"></div>` +
     `</div>`
   );
 }
